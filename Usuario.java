48b95f53591d11110548108294c916f7cb8e59a1
gabivra21 <162526494+gabivra21@users.noreply.github.com>
2024-04-18 12:18:32 -0300

    Update Usuario.java

--- Usuario.java
+++ Usuario.java
@@ -1,10 +1,36 @@
+public class VideoDigital extends ItemBibliotecaDigital implements Baixavel,Visualizavel{
+    protected boolean visualizado;
+    protected boolean baixado;
 
-public class Usuario {
-    protected String nome;
+    public VideoDigital(String titulo, String autor, boolean visualizado, boolean baixado){
 
-    public Usuario(String nome){
-        nome = nome;
+        super(titulo,autor);
+        this.visualizado = visualizado;
+        this.baixado = baixado;
+    }
+
+    @java.lang.Override
+    public String descricao() {
+       return "O video " + titulo + "\nfoi criado por " + autor;
     }
 
 
+
+
+    @Override
+    public void baixar() {
+        baixado = true;
+        System.out.println("Video baixado");
+
+
+    }
+
+
+
+    @Override
+    public void visualizar() {
+        visualizado = true;
+        System.out.println("Video visualizado");
+
+    }
 }
