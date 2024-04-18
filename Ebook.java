30eb02c5c06088654814008cc5685d5db939b852
gabivra21 <162526494+gabivra21@users.noreply.github.com>
2024-04-18 12:15:15 -0300

    Update Ebook.java

--- Ebook.java
+++ Ebook.java
@@ -1,20 +1,24 @@
 import java.util.ArrayList;
 
 public class Ebook extends ItemBibliotecaDigital implements Baixavel{
-    public Ebook(String titulo, String autor){
+    protected boolean baixado;
+    public Ebook(String titulo, String autor, boolean baixado){
+
         super(titulo,autor);
+        this.baixado = baixado;
+
     }
 
-    @java.lang.Override
-    public void descricao() {
-        System.out.println("O ebook "+ titulo);
-        System.out.println("Este ebook foi escrito por "+ autor);
+    @Override
+    public String descricao() {
+        return "O ebook " + titulo + "\nfoi escrito por " + autor;
     }
 
-    ArrayList ebooksBaixados = new ArrayList<Ebook>();
-    @java.lang.Override
-    public void baixar(Ebook ebook) {
-        ebooksBaixados.add(ebook);
+
+    @Override
+    public void baixar() {
+        baixado = true;
+        System.out.println("Ebook baixado");
 
     }
 }
