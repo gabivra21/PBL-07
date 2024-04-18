3d46cf36f0f3455b9668aef3bc5e073c49aad80b
gabivra21 <162526494+gabivra21@users.noreply.github.com>
2024-04-18 12:16:38 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,12 +1,26 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-import java.util.ArrayList;
+
+
 public class Main {
     public static void main(String[] args) {
-        Ebook ebook = new Ebook("Mentes Ansiosas","Ana Beatriz");
-        Ebook ebook1 = new Ebook("lalaland","kojo");
+        Ebook ebook = new Ebook("Mentes Ansiosas","Ana Beatriz", false);
+        Ebook ebook1 = new Ebook("lalaland","kojo",false);
 
-        VideoDigital videoDigital = new VideoDigital("Jacaca", "lucio");
+        VideoDigital videoDigital = new VideoDigital("Jacaca", "lucio",false, false);
+        VideoDigital videoDigital1 = new VideoDigital("brasil do brasil","bia",false, false);
+
+        System.out.println(ebook.descricao());
+        System.out.println(videoDigital.descricao());
+
+        System.out.println(ebook.baixado);
+        ebook.baixar();
+        System.out.println(ebook.baixado);
+
+
+
+
+
+
+
 
 
 
